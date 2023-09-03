eb39bd750aacc7490adeb4eea41cf54b44a9a852
Max Tymchii <samback@ukr.net>
2023-09-03 14:52:29 +0100

    Add Xcode project.

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Max Tymchii on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
