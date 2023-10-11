1e8e9162b5dd33d78e3fbd0f044e0a445772b4de
Max Tymchii <samback@ukr.net>
2023-10-11 10:50:04 +0100

    Optimise build time.

--- ShoppingList/ShoppingListApp.swift
+++ ShoppingList/ShoppingListApp.swift
@@ -7,11 +7,27 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        guard BuildChecker.arePreviewsRunning() == false else { return true }
+
+        return true
+    }
+}
+
 @main
 struct ShoppingListApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if BuildChecker.arePreviewsRunning() == false {
+                EmptyView()
+            } else {
+                ContentView()
+            }
         }
     }
 }
