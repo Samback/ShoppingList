ea284233b65036278071f34a419ed84bc269a31c
Max Tymchii <samback@ukr.net>
2023-11-04 11:43:03 +0000

    Add emojis initial implementation.

--- Emojis/Package.swift
+++ Emojis/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Emojis",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,8 +15,10 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture",
                  from: "1.3.0"),
-        .package(path: "../Theme")
+        .package(path: "../Theme"),
+        .package(url: "https://github.com/Samback/MCEmojiPicker", branch: "feature/viewController")
     ],
+
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -25,7 +27,9 @@ let package = Package(
         dependencies: [
             .product(name: "ComposableArchitecture",
                      package: "swift-composable-architecture"),
-            "Theme"
+            "Theme",
+            .product(name: "MCEmojiPicker",
+                     package: "MCEmojiPicker")
 
         ]),
         .testTarget(
