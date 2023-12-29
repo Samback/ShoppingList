0e6883b3e72dd877417192397732a2031314a179
Max Tymchii <samback@ukr.net>
2023-12-29 17:52:33 +0000

    Add UI extensions.

---
+++ Modules/Scanner/Sources/Scanner/Extensions/CGRect+extension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Tymchii on 29.12.2023.
+//
+
+import Foundation
+import UIKit
+
+extension CGRect {
+    var center: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+}
---
+++ Modules/Scanner/Sources/Scanner/Extensions/UIView+extension.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Tymchii on 10.12.2023.
+//
+
+import UIKit
+
+extension UIView {
+
+    static func spacer(size: CGFloat = .greatestFiniteMagnitude, for layout: NSLayoutConstraint.Axis = .horizontal) -> UIView {
+        let spacer = UIView()
+        // maximum width constraint
+        let spacerWidthConstraint = spacer.widthAnchor.constraint(equalToConstant: size) // or some very high constant
+        spacerWidthConstraint.priority = .defaultLow // ensures it will not "overgrow"
+        spacerWidthConstraint.isActive = true
+        return spacer
+    }
+
+}
